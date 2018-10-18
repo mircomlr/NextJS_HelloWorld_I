955afb31a99eb61eb4e4f98594ab8c24512ec229
Mirco Mueller <mirco.mueller@posteo.de>
2018-10-18 12:23:47 +0200

    Add PWA basic page structure using next.js II

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7be85636756f013b2d15";
+/******/ 	var hotCurrentHash = "d9b98832fb4b855885f8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;

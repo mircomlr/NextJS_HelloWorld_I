e54eb1f371e3c739a1a5108afd82743e66a66110
Mirco Mueller <mirco.mueller@posteo.de>
2018-10-18 12:28:24 +0200

    Add linking to Main Page

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d9b98832fb4b855885f8";
+/******/ 	var hotCurrentHash = "2aed3a53c5764ecae43c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
